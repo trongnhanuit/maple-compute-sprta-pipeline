fadf860b0223f0a8b4d78fed051464a0e29cb8d6
NhanLT <trongnhan.uit@gmail.com>
2024-08-30 11:09:11 +1000

    fix misspelling

--- scripts/maple_compute_sprta.sh
+++ scripts/maple_compute_sprta.sh
@@ -7,7 +7,7 @@ TREE_DIR=$2 # tree dir
 MAPLE_PATH=$3 # path to CMAPLE executable
 ML_TREE_PREFIX=$4 # The prefix of ML trees
 MAPLE_SPRTA_TREE_PREFIX=$5 # The prefix of trees with SPRTA computed by MAPLE
-MAPLE_PARAMS="---SPRTA --overwrite --networkOutput --keepInputIQtreeSupports --doNotImproveTopology --doNotReroot" # MAPLE params
+MAPLE_PARAMS="--SPRTA --overwrite --networkOutput --keepInputIQtreeSupports --doNotImproveTopology --doNotReroot" # MAPLE params
 PYPY_PATH="/scratch/dx61/tl8625/tmp/pypy3.10-v7.3.17-linux64/bin/pypy3.10"
 
 
