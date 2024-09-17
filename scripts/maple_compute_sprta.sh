63684cfc8b44b77895a82e37fb25ec91b858e10c
NhanLT <trongnhan.uit@gmail.com>
2024-09-17 16:55:04 +1000

    compute supports for branches with a length of zero

--- scripts/maple_compute_sprta.sh
+++ scripts/maple_compute_sprta.sh
@@ -10,16 +10,16 @@ MAPLE_SPRTA_TREE_PREFIX=$5 # The prefix of trees with SPRTA computed by MAPLE
 MODEL=$6 # Substitution model
 BLENGTHS_FIXED=$7 # keep blengths fixed or not
 PYPY_PATH=$8 # path to pypy
-PRINT_LESS_INFO_SEQS=$9 # print SPRTA for less-informative sequences
+ZERO_LENGTH_BRANCHES=$9 #  compute supports for branches with a length of zero
 
 BL_FIXED_OPT=""
 if [ "${BLENGTHS_FIXED}" = true ]; then
   BL_FIXED_OPT=" --blfix"
 fi
 
-PRINT_LESS_INFO_SEQS_OPT=""
-if [ "${PRINT_LESS_INFO_SEQS}" = true ]; then
-  PRINT_LESS_INFO_SEQS_OPT=" --supportForIdenticalSequences"
+ZERO_LENGTH_BRANCHES_OPT=""
+if [ "${ZERO_LENGTH_BRANCHES}" = true ]; then
+  ZERO_LENGTH_BRANCHES_OPT=" --supportFor0Branches --supportForIdenticalSequences"
 fi
 
 MAPLE_PARAMS="--SPRTA --overwrite --networkOutput --keepInputIQtreeSupports --doNotImproveTopology --doNotReroot" # MAPLE params
@@ -37,8 +37,8 @@ rm -f ${TREE_DIR}/${MAPLE_SPRTA_TREE_PREFIX}${aln}_nexusTree.tree
 for aln_path in "${ALN_DIR}"/*.maple; do
 	aln=$(basename "$aln_path")
     echo "Compute SPRTA (by MAPLE) for the tree ${ML_TREE_PREFIX}${aln}.treefile inferred from ${aln}"
-    echo "cd ${ALN_DIR} && ${PYPY_PATH} ${MAPLE_PATH} --input ${aln} --output ${ALN_DIR}/${MAPLE_SPRTA_TREE_PREFIX}${aln} --inputTree ${TREE_DIR}/${CMAPLE_SPRTA_TREE_PREFIX}${aln}.treefile --model ${MODEL} ${MAPLE_PARAMS} ${BL_FIXED_OPT} ${PRINT_LESS_INFO_SEQS_OPT}"
-    cd ${ALN_DIR} && ${PYPY_PATH} ${MAPLE_PATH} --input ${aln} --output ${ALN_DIR}/${MAPLE_SPRTA_TREE_PREFIX}${aln} --inputTree ${TREE_DIR}/${CMAPLE_SPRTA_TREE_PREFIX}${aln}.treefile --model ${MODEL} ${MAPLE_PARAMS} ${BL_FIXED_OPT} ${PRINT_LESS_INFO_SEQS_OPT}
+    echo "cd ${ALN_DIR} && ${PYPY_PATH} ${MAPLE_PATH} --input ${aln} --output ${ALN_DIR}/${MAPLE_SPRTA_TREE_PREFIX}${aln} --inputTree ${TREE_DIR}/${CMAPLE_SPRTA_TREE_PREFIX}${aln}.treefile --model ${MODEL} ${MAPLE_PARAMS} ${BL_FIXED_OPT} ${ZERO_LENGTH_BRANCHES_OPT}"
+    cd ${ALN_DIR} && ${PYPY_PATH} ${MAPLE_PATH} --input ${aln} --output ${ALN_DIR}/${MAPLE_SPRTA_TREE_PREFIX}${aln} --inputTree ${TREE_DIR}/${CMAPLE_SPRTA_TREE_PREFIX}${aln}.treefile --model ${MODEL} ${MAPLE_PARAMS} ${BL_FIXED_OPT} ${ZERO_LENGTH_BRANCHES_OPT}
     
     # move tree
     mv ${ALN_DIR}/${MAPLE_SPRTA_TREE_PREFIX}${aln}_nexusTree.tree ${TREE_DIR}
